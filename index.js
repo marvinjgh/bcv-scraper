b992da064d1d1c7315ecd5c4a44496e454b5f046
Marvin Gonzalez <marvinjgh@gmail.com>
2022-08-16 21:10:04 -0400

    remove file write

--- index.js
+++ index.js
@@ -10,17 +10,10 @@ async function main() {
   try {
     const { data } = await axios.get(urlBase);
     const $ = cheerio.load(data);
-    const now = new Date();
     const dollar = $(
       "#dolar > div > div > div.col-sm-6.col-xs-6.centrado > strong"
     );
 
-    fs.writeFileSync(
-      "c:\\users\\marvin\\desktop\\bcv.txt",
-      `${dollar.text().trim()} ${now.toLocaleDateString()}\n`,
-      { flag: "a+" },
-      (err) => {}
-    );
     console.log("Price of dollar", dollar);
   } catch (error) {
     console.log(error);
